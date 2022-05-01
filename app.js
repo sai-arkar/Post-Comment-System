0501a00623f3076d80781f2445f095a1da97ecd5
sai-arkar <iamsaiarkar@gmail.com>
2022-05-01 17:57:32 +0630

    1-5-2022 author model and signup

--- app.js
+++ app.js
@@ -5,6 +5,53 @@ const app = express();
 
 const uri = "mongodb+srv://root:root@cluster0.hl7kn.mongodb.net/Post-Comment-System?retryWrites=true&w=majority";
 
+const authRoutes = require("./routes/auth");
+const feedRoutes = require("./routes/feed");
+
+app.use(express.json());
+
+const fileStorge = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'images');
+    },
+    filename: (req, file, cb)=>{
+        // cb(null, file.originalname);
+       cb(null, uuidv4.v4() + '-' + file.originalname);
+    }
+});
+ 
+const fileFilter = (req, file, cb)=>{
+    if(
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg' 
+    ){
+        cb(null, true);
+    } else{
+        cb(null, false);
+    }
+}
+
+
+app.use(express.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(
+    multer({storage: fileStorge, fileFilter: fileFilter}).single('image')
+);
+
+app.use("/auth", authRoutes);
+app.use("/feed", feedRoutes);
+
+app.use((req, res, next)=>{
+     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+     res.setHeader('Access-Control-Allow-Credentials', true);
+     next();
+})
+
+
+
 mongoose.connect(uri)
     .then(()=>{
         console.log("Connected");
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+
+const Author = require("../models/author");
+
+router.put("/signup");
+
+
+module.exports = router;
